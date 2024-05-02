6bd8e23df0dea04df47b5a5ea800d52c6d96517b
Rafael M. da Silva <123322146+rafaelmdasilva@users.noreply.github.com>
2024-05-02 15:52:18 -0300

    Code optimization

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 'use strict'
 
+const url = 'https://unpkg.com/color-name-list@10.21.0/dist/colornames.bestof'
+
 const titleEle = document.querySelector('#title')
+const boxEle = document.querySelector('#box')
 const resultEle = document.querySelector('#result')
 const buttonEle = document.querySelector('#fetch')
 const nameEle = document.createElement('p')
@@ -9,33 +12,38 @@ const holderEle = document.createElement('p')
 
 nameEle.setAttribute('id', 'name')
 hexEle.setAttribute('id', 'hex')
+hexEle.style.fontFamily = 'Inconsolata'
+hexEle.style.textTransform = 'uppercase'
 
 holderEle.innerText = 'Go on. Give it a go.'
 buttonEle.innerText = 'Fetch!'
-
 resultEle.appendChild(holderEle)
 
+boxEle.style.transform = `translateY(-${titleEle.offsetHeight}px)`
+titleEle.style.transform = `translateY(-${boxEle.offsetHeight / 2}px)`
+//align elements
+
+let parsed
+fetch(url)
+    .then(response => { return response.json() })
+    // fetch and parse data
+    .then(data => { parsed = data })
+    // log parsed data
+
 buttonEle.addEventListener('click', () => {
-    fetch('https://unpkg.com/color-name-list@10.21.0/dist/colornames.bestof')
-        .then(response => { return response.json(); })
-        // fetch and parse data
-        .then(parsedData => {
-            // log parsed data
-            const parsedLength = Object.keys(parsedData).length;
-            let random = Math.round(Math.random() * parsedLength)
-            // caps random to object length
+    const parsedLength = Object.keys(parsed).length
+    let random = Math.round(Math.random() * parsedLength)
+    // caps random to object length
 
-            resultEle.appendChild(nameEle);
-            resultEle.appendChild(hexEle);
+    holderEle.remove()
+    resultEle.appendChild(nameEle)
+    resultEle.appendChild(hexEle)
 
-            nameEle.innerText = parsedData[random].name
-            hexEle.innerText = parsedData[random].hex
-        })
-        .then(() => {
-            holderEle.remove()
-            document.body.style.background = `linear-gradient(to bottom, ${hexEle.textContent}, #ffffff)`
-            titleEle.innerText = nameEle.textContent
-            buttonEle.style.background = hexEle.textContent
-        }
-        )
-})
+    nameEle.innerText = parsed[random].name
+    hexEle.innerText = parsed[random].hex
+    titleEle.innerText = nameEle.textContent
+
+    buttonEle.style.background = hexEle.textContent
+    document.body.style.background = `linear-gradient(to bottom, ${hexEle.textContent}, #ffffff)`
+}
+)
